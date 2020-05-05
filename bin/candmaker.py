67418795625fd18101eee16667b4ed47e4b4805a
Mateusz Malenta <mateusz.malenta@gmail.com>
2020-05-05 20:03:19 +0100

    Version for MeerTRAP
    Now fits MeerTRAP streaming processing. Added RabbitMQ support to pass the data between scripts. Additional processing with candmaker to get rid of RFI and better prepare the data.

--- bin/candmaker.py
+++ bin/candmaker.py
@@ -1,29 +1,152 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import logging
 import pathlib
-from multiprocessing import Pool
+import pika
 
 import numpy as np
-import pandas as pd
+from os import path
 from pysigproc import SigprocFile
-from candidate import Candidate, crop
-from h5plotter import plot_h5
+from candidate import *
+from gpu_utils import gpu_dedisp_and_dmt_crop
+from socket import gethostname
+from time import time
 
-logger = logging.getLogger()
+from mtcutils import core as mtcore
+from mtcutils import iqrm_mask
+
+logger = logging.getLogger("candmaker")
+
+class Candmaker:
+
+    def __init__(self, config):
+        self._verbose = config['verbose']
+        self._opt_dm = config['opt_dm']
+        self._time_size = config['time_size']
+        self._freq_size = config['freq_size']
+        self._cand_data = None
+        self._cand_h5 = ""
+        self._hostname = gethostname()
 
 
-def normalise(data):
-    """
-    Noramlise the data by unit standard deviation and zero median
-    :param data: data
-    :return:
-    """
-    data = np.array(data, dtype=np.float32)
-    data -= np.median(data)
-    data /= np.std(data)
-    return data
+    def Candmake(self):
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange='candidates_' + self._hostname,
+                                    exchange_type='direct',
+                                    durable=True)
+        
+        channel.queue_declare('fetch_' + self._hostname, durable=True)
+        channel.queue_bind('fetch_' + self._hostname, 'candidates_' + self._hostname)
+
+        channel.queue_declare('fetch_predictor_' + self._hostname, durable=True)
+        channel.queue_bind('fetch_predictor_' + self._hostname, 'candidates_' + self._hostname)
+
+        channel.basic_qos(prefetch_count=1)
+
+        channel.basic_consume(queue='fetch_' + self._hostname, auto_ack=False, on_message_callback=self.Cand2H5)
+        channel.start_consuming()
+
+    def Cand2H5(self, ch, method, properties, body):
+
+        self._cand_data = json.loads(body.decode('utf8'))
+        # Extra variables
+        base_dir = self._cand_data['base_dir']
+        beam_dir = self._cand_data['beam_dir']
+        # TODO: remember to add mask file to the published message
+        mask_file = self._cand_data['mask_file']
+        # Usual FETCH variables
+        fil_name = self._cand_data['filterbank']
+        snr = float(self._cand_data['cand_snr'])
+        width = 2 ** float(self._cand_data['cand_width'])
+        dm = float(self._cand_data['cand_dm'])
+        tcand = float(self._cand_data['cand_start_time'])
+        label = 1
+        # TODO: remember to add beam information to the published message
+        beam = self._cand_data['cand_beam']
+        beam_type = self._cand_data['cand_beam_type']
+
+        logger.info("Processing file % s", (fil_name))
+
+        filobj = SigprocFile(path.join(base_dir, beam_dir, fil_name))
+        logger.info("Creating 'mask' for %d channels", filobj.nchans)
+        mask = np.zeros(filobj.nchans, dtype=np.bool)
+
+        cand = Candidate(path.join(base_dir, beam_dir, fil_name), snr=snr, width=width, dm=dm, label=label, tcand=tcand, kill_mask=mask)
+        cand.get_chunk()
+        cand.fp.close()
+
+        # NOTE: mean is currently dummy variables that contain per-channel mean and standard deviation of the original data.
+        # Should we include that in the archive?
+        scaled, mean, std = mtcore.normalise(cand.data.T)
+        # NOTE: Per note above - mtcutils iqrm_mask() returns logical mask
+        # Should we include iqrm mask in the archive?
+        mask = iqrm_mask(std, maxlag=3)
+        scaled[mask] = 0
+        cand.data = scaled.T
+        
+        dedisp_start = time()
+
+        cand=gpu_dedisp_and_dmt_crop(cand)
+        cand.dedispersed=resize(cand.dedispersed, (256,256))
+
+        cand.dmt = self.__NormaliseClip(cand.dmt)
+        cand.dedispersed = self.__NormaliseClip(cand.dedispersed, 3)
+        
+        dedisp_end = time()
+        logger.info("Dedispersion took %.2f seconds", (dedisp_end - dedisp_start))
+
+        fmtdm = "{:.2f}".format(dm)
+        self._cand_h5 = path.join(beam_dir, str(tcand) + '_DM_' + fmtdm + '_beam_' + str(beam) + beam_type + fil_name +  '.hdf5')
+        
+        print(self._cand_h5)
+
+        fout = cand.save_h5(self._cand_h5)
+        if self._verbose:
+            print("Created file %s" % (self._cand_h5))
+
+        pred_json = {
+            'beam_dir': beam_dir,
+            'filterbank': fil_name,
+            'candidate_h5': self._cand_h5
+        }
+
+        pika_properties = pika.BasicProperties(content_type='application/json',
+                                                content_encoding='utf-8',
+                                                delivery_mode=2)
+
+        # TODO: Change it to self._hostname
+        self._hostname = gethostname()
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        ch.basic_publish(exchange='candidates_' + self._hostname,
+                                routing_key='fetch_predictor_' + self._hostname,
+                                body=json.dumps(pred_json),
+                                properties=pika_properties)
+
+
+    def __NormaliseClip(self, data, clip_range=None):
+        """
+        Noramlise the data by unit standard deviation and zero median.
+        Clip the resulting data to withing 3 sigma.
+        :param data: data
+        :return:
+        """
+        data = np.array(data, dtype=np.float32)
+        median = np.median(data)
+        std = np.std(data)
+        logging.debug(f'Data median: {median}')
+        logging.debug(f'Data std: {std}')
+        data -= median
+        data /= std
+
+        if clip_range != None:
+            data = np.clip(data, -1.0 * clip_range, clip_range) 
+        return data
 
 
 def cand2h5(cand_val):
@@ -100,34 +223,33 @@ def cand2h5(cand_val):
         plot_h5(fout, show=False, save=True, detrend=False)
     return None
 
+def main():
 
-if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', help='Be verbose', action='store_true')
-    parser.add_argument('-fs', '--frequency_size', type=int, help='Frequency size after rebinning', default=256)
+    parser.add_argument('-fs', '--freq_size', type=int, help='Frequency size after rebinning', default=256)
     parser.add_argument('-ts', '--time_size', type=int, help='Time length after rebinning', default=256)
-    parser.add_argument('-c', '--cand_param_file', help='csv file with candidate parameters', type=str, required=True)
-    parser.add_argument('-n', '--nproc', type=int, help='number of processors to use in parallel (default: 2)',
-                        default=2)
-    parser.add_argument('-p', '--plot', dest='plot', help='To display and save the candidates plots',
-                        action='store_true')
-    parser.add_argument('-o', '--fout', help='Output file directory for candidate h5', type=str)
     parser.add_argument('-opt', '--opt_dm', dest='opt_dm', help='Optimise DM', action='store_true', default=False)
-    values = parser.parse_args()
+    arguments = parser.parse_args()
+
+    configuration = {
+        'verbose': arguments.verbose,
+        'opt_dm': arguments.opt_dm,
+        'time_size': arguments.time_size,
+        'freq_size': arguments.freq_size,
+    }
 
     logging_format = '%(asctime)s - %(funcName)s -%(name)s - %(levelname)s - %(message)s'
 
-    if values.verbose:
+    if arguments.verbose:
         logging.basicConfig(level=logging.DEBUG, format=logging_format)
     else:
         logging.basicConfig(level=logging.INFO, format=logging_format)
 
-    cand_pars = pd.read_csv(values.cand_param_file,
-                            names=['fil_file', 'snr', 'stime', 'dm', 'width', 'label', 'kill_mask_path'])
-    process_list = []
-    for index, row in cand_pars.iterrows():
-        process_list.append(
-            [row['fil_file'], row['snr'], 2 ** row['width'], row['dm'], row['label'], row['stime'],
-             row['kill_mask_path'], values])
-    with Pool(processes=values.nproc) as pool:
-        pool.map(cand2h5, process_list, chunksize=1)
+    candmaker = Candmaker(configuration)
+    candmaker.Candmake()
+
+
+if __name__ == '__main__':
+    main()
+
--- bin/predict.py
+++ bin/predict.py
@@ -8,68 +8,106 @@ import string
 
 import numpy as np
 import pandas as pd
+import pika
+import json
 
+from socket import gethostname
 from fetch.data_sequence import DataGenerator
 from fetch.utils import get_model
+from string import ascii_lowercase
+from numba import cuda
 
-logger = logging.getLogger(__name__)
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+
+class Predictor:
+
+    def __init__(self, config):
+
+        tf_config = tf.ConfigProto()
+        tf_config.gpu_options.per_process_gpu_memory_fraction = 0.25
+        set_session(tf.Session(config=tf_config))
+
+        cuda.select_device(0)
+
+        self._verbose = config['verbose']
+        self._threshold = config['threshold']
+
+        if config['model'] not in list(ascii_lowercase)[:11]:
+            raise ValueError('Model only range from a -- j.')
+        else:
+            self._model = get_model(config['model'])
+
+        if config['gpu'] >= 0:
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(config['gpu'])
+        else:
+            os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+    def Predict(self):
+
+        hostname = gethostname()
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+
+        channel.exchange_declare(exchange='candidates_' + hostname,
+                                    exchange_type='direct',
+                                    durable=True)
+
+        channel.queue_declare('fetch_predictor_' + hostname, durable=True)
+        channel.queue_bind('fetch_predictor_' + hostname, 'candidates_' + hostname)
+
+        channel.basic_qos(prefetch_count=1)
+
+        channel.basic_consume(queue='fetch_predictor_' + hostname, auto_ack=False, on_message_callback=self.GetPrediction)
+        channel.start_consuming()
+
+    def GetPrediction(self, ch, method, properties, body):
+        
+        cand_data = json.loads(body.decode('utf8'))
+        cand_h5 = cand_data['candidate_h5']
+
+        if self._verbose:
+            print("Running predictor for file %s..." % (cand_h5))
+
+        cand_datagen = DataGenerator(list_IDs = [cand_h5], labels=[0], shuffle=False, noise=False, batch_size=1)
+
+        cand_probs = self._model.predict_generator(generator=cand_datagen, max_queue_size=1, verbose=1, use_multiprocessing=False,
+                                    workers=1, steps=len(cand_datagen))
+
+        hostname = gethostname()
+
+        for idx, cand in enumerate(cand_probs):
+
+            print(idx, cand)
+            results_dict = {}
+            results_dict['beam_dir'] = cand_data['beam_dir']
+            results_dict['filterbank'] = cand_data['filterbank']
+            results_dict['candidate'] = cand_data['candidate_h5']
+            results_dict['probability'] = cand[1].item(0)
+            results_dict['label'] = int(np.round(cand[1].item(0) >= self._threshold))
+            print(results_dict)
+        
+            ch.basic_publish(exchange='candidates_' + hostname,
+                            routing_key='fetch',
+                            body=json.dumps(results_dict))
+
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fast Extragalactic Transient Candiate Hunter (FETCH)",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-v', '--verbose', help='Be verbose', action='store_true')
     parser.add_argument('-g', '--gpu_id', help='GPU ID (use -1 for CPU)', type=int, required=False, default=0)
-    parser.add_argument('-n', '--nproc', help='Number of processors for training', default=4, type=int)
-    parser.add_argument('-c', '--data_dir', help='Directory with candidate h5s.', required=True, type=str)
-    parser.add_argument('-b', '--batch_size', help='Batch size for training data', default=8, type=int)
     parser.add_argument('-m', '--model', help='Index of the model to train', required=True)
-    parser.add_argument('-p', '--probability', help='Detection threshold', default=0.5, type=float)
-    args = parser.parse_args()
+    parser.add_argument('-t', '--threshold', help='Detection threshold', default=0.5, type=float)
+    arguments = parser.parse_args()
 
-    logging_format = '%(asctime)s - %(funcName)s -%(name)s - %(levelname)s - %(message)s'
+    configuration = {
+        'verbose': arguments.verbose,
+        'gpu': arguments.gpu_id,
+        'model': arguments.model,
+        'threshold': arguments.threshold
+    }
 
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG, format=logging_format)
-    else:
-        logging.basicConfig(level=logging.INFO, format=logging_format)
-
-    if args.model not in list(string.ascii_lowercase)[:11]:
-        raise ValueError(f'Model only range from a -- j.')
-
-    if args.gpu_id >= 0:
-        os.environ["CUDA_VISIBLE_DEVICES"] = f'{args.gpu_id}'
-    else:
-        os.environ["CUDA_VISIBLE_DEVICES"] = ""
-
-    if args.nproc == 1:
-        use_multiprocessing = True
-        logging.info(f'Using multiprocessing with {args.nproc} workers')
-    else:
-        use_multiprocessing = False
-
-    if args.model not in list(string.ascii_lowercase)[:11]:
-        raise ValueError(f'Model only range from a -- j.')
-
-    cands_to_eval = glob.glob(f'{args.data_dir}/*h5')
-
-    if len(cands_to_eval) == 0:
-        raise FileNotFoundError(f"No candidates to evaluate.")
-
-    logging.debug(f'Read {len(cands_to_eval)} candidates')
-
-    # Get the data generator, make sure noise and shuffle are off.
-    cand_datagen = DataGenerator(list_IDs=cands_to_eval, labels=[0] * len(cands_to_eval), shuffle=False, noise=False,
-                                 batch_size=args.batch_size)
-
-    model = get_model(args.model)
-
-    # get's get predicting
-    probs = model.predict_generator(generator=cand_datagen, verbose=1, use_multiprocessing=use_multiprocessing,
-                                    workers=args.nproc, steps=len(cand_datagen))
-
-    # Save results
-    results_dict = {}
-    results_dict['candidate'] = cands_to_eval
-    results_dict['probability'] = probs[:,1]
-    results_dict['label'] = np.round(probs[:, 1] >= args.probability)
-    results_file = args.data_dir + f'/results_{args.model}.csv'
-    pd.DataFrame(results_dict).to_csv(results_file)
+    predictor = Predictor(configuration)
+    predictor.Predict()
